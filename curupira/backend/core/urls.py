59b4b4e72cc70ac209e4395d7282da5f2510315c
Fernando Yang <fernando38954@gmail.com>
2025-10-14 21:34:13 -0300

    feat: create API to get activity list information

--- curupira/backend/core/urls.py
+++ curupira/backend/core/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from activities.views import list_categories, activities_by_category
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path('api/categories/', list_categories, name='list_categories'),
+    path('api/categories/<int:category_id>/activities/', activities_by_category, name='activities_by_category'),
 ]
