639096ea9e1486e43a46ef1ce84daf675f8ffda1
Xnths <jonathascastilhoes@gmail.com>
2025-11-06 09:16:43 -0300

    fix: prod health check

--- curupira/backend/core/views.py
+++ curupira/backend/core/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from core.API.test_organizer import ExampleDataOrganizer
+from backend.core.API.test_organizer import ExampleDataOrganizer
 
 def test_api_view(request):
     organizer = ExampleDataOrganizer()
